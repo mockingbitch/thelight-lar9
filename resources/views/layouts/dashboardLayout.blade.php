2de50b385aad24dee633f134810d7e6da45248e7
chuyen_nguyen <nguyen.chuyen@jvb-corp.com>
2022-12-22 15:27:20 +0700

    favicon

--- resources/views/layouts/dashboardLayout.blade.php
+++ resources/views/layouts/dashboardLayout.blade.php
@@ -11,7 +11,7 @@ use App\Constants\DashboardConstant;
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="{{asset('upload/images/favicon.png')}}">
   <title>
     The Light
   </title>
--- resources/views/layouts/homeLayout.blade.php
+++ resources/views/layouts/homeLayout.blade.php
@@ -14,6 +14,7 @@ use App\Constants\UserConstant;
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/png" href="{{asset('upload/images/favicon.png')}}">
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 		<title>The Light</title>
